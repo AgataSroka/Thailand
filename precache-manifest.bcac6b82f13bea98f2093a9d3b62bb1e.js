0e2d11b4be2c26f0f02dc9c9073038f9c2ce0ced
Agata Połetek <poletekagata@gmail.com>
2019-09-15 21:37:35 +0200

    Updates

---
+++ precache-manifest.bcac6b82f13bea98f2093a9d3b62bb1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0dbe2a46ea0b253a3c41400e5306e0",
+    "url": "/AgataSroka/Thailand/index.html"
+  },
+  {
+    "revision": "817e2760bc8316629bf2",
+    "url": "/AgataSroka/Thailand/static/css/main.6bc3410e.chunk.css"
+  },
+  {
+    "revision": "b22f9ad85772ac3a1f2e",
+    "url": "/AgataSroka/Thailand/static/js/2.bf922b9a.chunk.js"
+  },
+  {
+    "revision": "817e2760bc8316629bf2",
+    "url": "/AgataSroka/Thailand/static/js/main.1d07280d.chunk.js"
+  },
+  {
+    "revision": "1e9d7e7f638519d5527e",
+    "url": "/AgataSroka/Thailand/static/js/runtime~main.f896cbf8.js"
+  },
+  {
+    "revision": "f44727ffb16674f9dbee3bedfc2565ed",
+    "url": "/AgataSroka/Thailand/static/media/main_2.f44727ff.jpg"
+  },
+  {
+    "revision": "9987ffff0a2bbeb8fcbcbbef1b05757b",
+    "url": "/AgataSroka/Thailand/static/media/map_2.9987ffff.jpg"
+  },
+  {
+    "revision": "d04080ba0523acd2a12303fe59f74f8b",
+    "url": "/AgataSroka/Thailand/static/media/map_3.d04080ba.jpg"
+  },
+  {
+    "revision": "3bbf0b24600d929c8e4e7544eb468c93",
+    "url": "/AgataSroka/Thailand/static/media/map_4.3bbf0b24.jpg"
+  }
+]);
