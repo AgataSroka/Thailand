ed066b6ae9db08b1c775957154d23260b53e49bd
Agata Połetek <poletekagata@gmail.com>
2019-12-20 12:25:49 +0100

    responsive subpage - in progress

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,15 @@
 import React, {Component} from "react";
 import '../../Styles/Main.scss';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGlobeAsia} from "@fortawesome/free-solid-svg-icons";
 
 class Main extends Component {
     render() {
         return (
             <section className='kenburns-top-right'>
+                <div className='font_position'>
+                {/*<FontAwesomeIcon icon={faGlobeAsia} size="10x" className="faGlobe" color='white'/>*/}
+                </div>
             </section>
         )
     }
